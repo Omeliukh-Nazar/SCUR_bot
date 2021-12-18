bd89f87b79b1a3e079546da2168ecd67bcfe88a1
Trudaga <nazar.omeliukh@gmail.com>
2021-12-19 01:20:58 +0200

    Added login

--- Program.cs
+++ Program.cs
@@ -1,16 +1,35 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace SCUR_bot
 {
     public class Program
     {
+        private DiscordSocketClient _client;
         public static Task Main(string[] args)
             => new Program().MainAsync();
 
         public async Task MainAsync()
         {
-            throw new NotImplementedException();
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            var token = File.ReadAllText("token.txt");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
